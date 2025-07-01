fc36980b79dce98baad7f9ac0ee4ee98881b834a
Jourdany <jourdanyrayssa@gmail.com>
2025-07-01 10:46:10 -0300

    correção

--- src/script.js
+++ src/script.js
@@ -4,25 +4,25 @@ function calcularRankeadas(vitorias, derrotas){
   let nivel = "";
 
   if (vitorias < 10){
-    nivel = "ferro";
+    nivel = "Ferro";
   }
   else if (vitorias >= 11 &&  vitorias <= 20){
-    nivel = "bronze";
+    nivel = "Bronze";
   }
   else if (vitorias >=21 && vitorias <= 50){
-    nivel = prata;
+    nivel = "Prata";
   }
    else if (vitorias >=51 && vitorias <= 80){
-    nivel = ouro;
+    nivel = "Ouro";
   }
    else if (vitorias >=81 && vitorias <= 90){
-    nivel = Diamante;
+    nivel = "Diamante";
   }
    else if (vitorias >=91 && vitorias <= 100){
-    nivel = Lendário;
+    nivel = "Lendário";
   }
    else if (vitorias <= 101){
-    nivel = Imortal;
+    nivel = "Imortal";
   }  
   
   console.log(`O Herói tem de saldo de **${saldoVitorias}** está no nível de **${nivel}**`);
@@ -37,25 +37,25 @@ function calcularRankeadas (vitorias, derrotas){
 
   switch (true){
     case (vitorias < 10):
-      nivel = "ferro;"
+      nivel = "Ferro;"
       break;
     case (vitorias >= 11 && vitorias <= 20):
-      nivel = "bronze";
+      nivel = "Bronze";
       break;
       case (vitorias >= 21 && vitorias <= 50):
-      nivel = "prata";
+      nivel = "Prata";
       break;
       case (vitorias >= 31 && vitorias <= 40):
-      nivel = "ouro";
+      nivel = "Ouro";
       break;
       case (vitorias >= 51 && vitorias <= 80):
-      nivel = "diamante";
+      nivel = "Diamante";
       break;
       case (vitorias >= 81 && vitorias <= 90):
-      nivel = "lendário";
+      nivel = "Lendário";
       break;
       case (vitorias >= 101):
-      nivel = "imortal";
+      nivel = "Imortal";
       break;
       default: 
       nivel = "ainda não tem classificação";      
