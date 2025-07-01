36049588f565506b236a7ed6f42451d976be4ebe
Jourdany <jourdanyrayssa@gmail.com>
2025-07-01 10:35:37 -0300

    projeto dio 2

---
+++ src/script.js
@@ -0,0 +1,65 @@
+// COM IF E ELSE IF
+function calcularRankeadas(vitorias, derrotas){
+  let saldoVitorias = vitorias - derrotas;
+  let nivel = "";
+
+  if (vitorias < 10){
+    nivel = "ferro";
+  }
+  else if (vitorias >= 11 &&  vitorias <= 20){
+    nivel = "bronze";
+  }
+  else if (vitorias >=21 && vitorias <= 50){
+    nivel = prata;
+  }
+   else if (vitorias >=51 && vitorias <= 80){
+    nivel = ouro;
+  }
+   else if (vitorias >=81 && vitorias <= 90){
+    nivel = Diamante;
+  }
+   else if (vitorias >=91 && vitorias <= 100){
+    nivel = Lendário;
+  }
+   else if (vitorias <= 101){
+    nivel = Imortal;
+  }  
+  
+  console.log(`O Herói tem de saldo de **${saldoVitorias}** está no nível de **${nivel}**`);
+    return { saldoVitorias, nivel };
+}
+
+//OUTRA FORMA DE FAZER O MESMO ALGORÍTMO COM SWITCH CASE:
+
+function calcularRankeadas (vitorias, derrotas){
+  let saldoVitorias = vitorias - derrotas;
+  let nivel = "";
+
+  switch (true){
+    case (vitorias < 10):
+      nivel = "ferro;"
+      break;
+    case (vitorias >= 11 && vitorias <= 20):
+      nivel = "bronze";
+      break;
+      case (vitorias >= 21 && vitorias <= 50):
+      nivel = "prata";
+      break;
+      case (vitorias >= 31 && vitorias <= 40):
+      nivel = "ouro";
+      break;
+      case (vitorias >= 51 && vitorias <= 80):
+      nivel = "diamante";
+      break;
+      case (vitorias >= 81 && vitorias <= 90):
+      nivel = "lendário";
+      break;
+      case (vitorias >= 101):
+      nivel = "imortal";
+      break;
+      default: 
+      nivel = "ainda não tem classificação";      
+  }
+  console.log(`O Herói tem de saldo de **${saldoVitorias}** está no nível de **${nivel}**`);
+    return { saldoVitorias, nivel };
+}
